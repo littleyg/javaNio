7df3d8ab7646c8b54738d20f2763449d7fa21b63
willieyin <willieyin@tencent.com>
2021-12-25 18:14:16 +0800

    buffer数据如果不能写完则注册OP_WRITE事件，后续处理

--- main/java/com/example/demo/FileClient.java
+++ main/java/com/example/demo/FileClient.java
@@ -3,12 +3,10 @@ package com.example.demo;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.LongBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
@@ -25,42 +23,71 @@ public class FileClient {
         long size = fileChannel.size();
         System.out.println("size = " + size);
         buffer.putLong(size);
-//        System.out.println("lb.position() = " + buffer.position());
         buffer.flip();
         tcpSocket.write(buffer);
         buffer.clear();
-        while (fileChannel.read(buffer) != -1) {
-            buffer.flip();
-            int remaining = buffer.remaining();
-            System.out.println("buffer.remaining() = " + remaining);
-            int writed = tcpSocket.write(buffer);
 
-            System.out.println("write = " + writed);
-           if (writed<remaining){
-               //还要继续写完的。。不然直接clear就可能导致会有问题
-           }
-            buffer.clear();
-        }
+        writeFileToSocket(tcpSocket, fileChannel, buffer, selector);
         System.out.println(" 已经发送完图片，等服务器的答复 " );
-        fileChannel.close();
         while (selector.select() > 0) {
             Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
             while (iterator.hasNext()) {
                 SelectionKey selectionKey = iterator.next();
                 iterator.remove();
+                SocketChannel channel = (SocketChannel) selectionKey.channel();
                 if (selectionKey.isReadable()) {
-                    SocketChannel channel = (SocketChannel) selectionKey.channel();
                     ByteBuffer responseBuffer = ByteBuffer.allocate(1024);
                     int readBytes = channel.read(responseBuffer);
                     if (readBytes > 0) {
                         responseBuffer.flip();
                         System.out.println("服务器回复了，"+new String(responseBuffer.array(), 0, readBytes));
                         channel.close();
+                        fileChannel.close();
                         return;
                     }
+                }else if (selectionKey.isWritable()){
+                    //将剩下的没有写完的数据继续写完
+                    continueWriteFileToSocket(selectionKey,channel, fileChannel, buffer, selector);
                 }
             }
         }
+    }
+
+    private static void writeFileToSocket(SocketChannel tcpSocket, FileChannel fileChannel, ByteBuffer buffer, Selector selector) throws IOException {
+        while (fileChannel.read(buffer) != -1) {
+            buffer.flip();
+            int remaining = buffer.remaining();
+            System.out.println("buffer.remaining() = " + remaining);
+            int writed = tcpSocket.write(buffer);
+            System.out.println("write = " + writed);
+           if (writed<remaining){
+               tcpSocket.register(selector,SelectionKey.OP_WRITE);
+               break;
+           }
+            buffer.clear();
+        }
+    }
+
+    private static void continueWriteFileToSocket(SelectionKey key,SocketChannel tcpSocket, FileChannel fileChannel, ByteBuffer buffer, Selector selector) throws IOException {
+        //buffer在之前kennel被先get了一部分，position的位置已经发生了改变
+        int writed = tcpSocket.write(buffer);
+        if (writed<buffer.remaining()){
+           return;
+        }
+        buffer.clear();
+        //如果能写完，就可以继续往buffer里面put数据并继续发送到网上了
+        while (fileChannel.read(buffer) != -1) {
+            buffer.flip();
+            int remaining = buffer.remaining();
+            writed = tcpSocket.write(buffer);
+            System.out.println("write = " + writed);
+            if (writed<remaining){
+                return;
+            }else {
+                buffer.clear();
+            }
+        }
+        key.cancel();
     }
 }
 
